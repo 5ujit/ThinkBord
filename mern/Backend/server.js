fc2a1e305915c374d0c64a4477dc1c291f2053bf
5ujit <sksujit021@gmail.com>
2025-10-02 22:26:07 +0530

    Update server.js

--- mern/Backend/server.js
+++ mern/Backend/server.js
@@ -2,8 +2,10 @@ import express from "express";
 
 const app = express();
 
+app.listen(3000, () => {
+  console.log("Server is running on port 3000");
+});
 
-app.listen(5001,()=>{
-    console.log();
-    
-})
+app.get("/", (req, res) => {
+  res.send("Hello, World!");
+}); 
