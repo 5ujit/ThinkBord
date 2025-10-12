c517dc066c921fa7966cf29d870d041806951aaf
5ujit <sksujit021@gmail.com>
2025-10-12 23:56:53 +0530

    Update notesController.js

--- MERN-CRUD/Backend/src/controllers/notesController.js
+++ MERN-CRUD/Backend/src/controllers/notesController.js
@@ -9,9 +9,17 @@ export async function getAllNotes(_, res) {
 }
 
 
-export function createNote ( req, res){
-    res.status(201).json({messge:"Note created successfully !"});
+export async function getNoteById(req, res) {
+  try {
+    const note = await Note.findById(req.params.id);
+    if (!note) return res.status(404).json({ message: "Note not found!" });
+    res.json(note);
+  } catch (error) {
+    console.error("Error in getNoteById controller", error);
+    res.status(500).json({ message: "Internal server error" });
+  }
 }
+
 
 export function updateNote (req, res){
     res.status(200).json({message: "Note update successfully"})
