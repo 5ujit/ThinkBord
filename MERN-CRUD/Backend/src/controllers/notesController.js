165b47492ee3a69416e522dd847a4a2b1da7fd2f
5ujit <sksujit021@gmail.com>
2025-10-28 14:40:04 +0530

    Update notesController.js

--- MERN-CRUD/Backend/src/controllers/notesController.js
+++ MERN-CRUD/Backend/src/controllers/notesController.js
@@ -1,11 +1,23 @@
-export function getAllNotes(req, res){
-  res.status(200).send("you just fetech tghe notes")
+export async function getAllNotes(_, res) {
+  try {
+    const notes = await Note.find().sort({ createdAt: -1 }); // -1 will sort in desc. order (newest first)
+    res.status(200).json(notes);
+  } catch (error) {
+    console.error("Error in getAllNotes controller", error);
+    res.status(500).json({ message: "Internal server error" });
+  }
 }
 
-export function createNote(req,res){
-   res.status(201).json({message: "Note created successfully "})
+export async function getNoteById(req, res) {
+  try {
+    const note = await Note.findById(req.params.id);
+    if (!note) return res.status(404).json({ message: "Note not found!" });
+    res.json(note);
+  } catch (error) {
+    console.error("Error in getNoteById controller", error);
+    res.status(500).json({ message: "Internal server error" });
+  }
 }
-
 export function updateNote(req, res){
   res.status(200).json({message:"Note deleted successfully"})
 }
