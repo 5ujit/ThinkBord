116d797e0472a6632ba1672128041f46421845aa
5ujit <sksujit021@gmail.com>
2025-11-01 11:10:35 +0530

    Update notesController.js

--- MERN-CRUD/Backend/src/controllers/notesController.js
+++ MERN-CRUD/Backend/src/controllers/notesController.js
@@ -1,36 +1,40 @@
-export async function getAllNotes(_, res) {
-  try {
-    const notes = await Note.find().sort({ createdAt: -1 }); // -1 will sort in desc. order (newest first)
-    res.status(200).json(notes);
-  } catch (error) {
-    console.error("Error in getAllNotes controller", error);
-    res.status(500).json({ message: "Internal server error" });
-  }
-}
-export async function getNoteById(req, res) {
-  try {
-    const note = await Note.findById(req.params.id);
-    if (!note) return res.status(404).json({ message: "Note not found!" });
-    res.json(note);
-  } catch (error) {
-    console.error("Error in getNoteById controller", error);
-    res.status(500).json({ message: "Internal server error" });
-  }
+export async function getAllNotes(req, res) {
+  res.status(200).send("Yes you fetched the notes ");
 }
 
 export async function createNote(req, res) {
-  try {
-    const { title, content } = req.body;
-    const note = new Note({ title, content });
+  res.status(500).json({ message: "Notes created successfully" });
+}
+const updateNotes = async (req, res) => {
+  res.status(200).json({ message: "Note updated successfully" });
+};
 
-    const savedNote = await note.save();
-    res.status(201).json(savedNote);
-  } catch (error) {
-    console.error("Error in createNote controller", error);
-    res.status(500).json({ message: "Internal server error" });
-  }
+export function deleteNote(req, res) {
+  res.status(200).json({ message: "Note deleted successfully!  " });
 }
 
-export function deleteNote(req,res){
-  res.status(200).json({message: "Note deleted successfully!  "});
-}
+export { updateNotes };
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*
+
+const updateNotes = async (req, res) => {
+  res.status(200).json({ message: "Note updated successfully" });
+};
+
+export { updateNotes };
+
+*/
