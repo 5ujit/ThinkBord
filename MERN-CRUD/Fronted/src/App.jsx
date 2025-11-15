97a42d56d5e4d2b3c76f8ceee3c31bd2001a3c56
5ujit <sksujit021@gmail.com>
2025-11-15 23:42:03 +0530

    forntend

--- MERN-CRUD/Fronted/src/App.jsx
+++ MERN-CRUD/Fronted/src/App.jsx
@@ -1,13 +1,23 @@
-import React from 'react'
+import React from "react";
+import Routers from "react-router;";
+import Route from "react-router;";
+import Homepage from "./pages/Homepage";
+import CreatePage from "./pages/CreatePage";
+import NoteDetailPage from "./pages/CreatePage";
 
 const App = () => {
-
-  
   return (
     <div>
-      
-    </div>
-  )
-}
+      <button onClick={()=>toast.success("congraats")  }></button>
+      <Routers>
+        <Route path="/" element={Homepage} />
+        <Route path="/create" element={CreatePage} />
+        <Route path="/note/:id" element={NoteDetailPage} />
+        
 
-export default App
+      </Routers>
+    </div>
+  );
+};
+
+export default App;
