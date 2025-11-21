059c5e9fc9ec3ef2d9ebfc280a97e886196ea8b9
5ujit <sksujit021@gmail.com>
2025-11-21 23:47:56 +0530

    .

--- MERN-CRUD/Fronted/src/App.jsx
+++ MERN-CRUD/Fronted/src/App.jsx
@@ -1,19 +1,21 @@
+// App.jsx
+import { Routes, Route } from "react-router-dom";
+import { toast } from "react-hot-toast";
 
 import Homepage from "./pages/Homepage";
 import CreatePage from "./pages/CreatePage";
-import NoteDetailPage from "./pages/CreatePage";
+import NoteDetailPage from "./pages/NoteDetailPage"; // <-- fix this path
 
 const App = () => {
   return (
     <div>
-      <button onClick={()=>toast.success("congraats")  }></button>
-      <Routers>
-        <Route path="/" element={Homepage} />
-        <Route path="/create" element={CreatePage} />
-        <Route path="/note/:id" element={NoteDetailPage} />
-        
+      <button onClick={() => toast.success("Congrats!")}>Click</button>
 
-      </Routers>
+      <Routes>
+        <Route path="/" element={<Homepage />} />
+        <Route path="/create" element={<CreatePage />} />
+        <Route path="/note/:id" element={<NoteDetailPage />} />
+      </Routes>
     </div>
   );
 };
--- MERN-CRUD/Fronted/src/pages/NoteDetailPage.jsx
+++ MERN-CRUD/Fronted/src/pages/NoteDetailPage.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const NoteDeatailPage = () => {
   return (
     <div>
-      
+      this is note detail page
     </div>
   )
 }
