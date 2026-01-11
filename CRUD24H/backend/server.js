cb1f6c36036202172dff59200337c64519efb003
5ujit <sksujit021@gmail.com>
2026-01-11 23:48:26 +0530

    Update server.js

--- CRUD24H/backend/server.js
+++ CRUD24H/backend/server.js
@@ -6,6 +6,12 @@ app.get("/api/notes",(req,res)=>{
     res.send("yoou got 5 notes from the server")
 })
 
+
+
+app.post("/api/notes",(req,res)=>{
+    res.send("note is added successfully")
+})
+
 app.listen(5001,()=>{
     console.log("server started on port : 5001");
     
