307027889dbc206a0c3122d97830318d9045fa7f
5ujit <sksujit021@gmail.com>
2026-01-12 23:52:45 +0530

    Update server.js

--- CRUD24H/backend/server.js
+++ CRUD24H/backend/server.js
@@ -12,6 +12,14 @@ app.post("/api/notes",(req,res)=>{
     res.send("note is added successfully")
 })
 
+app.post("/api/notes",(req,res)=>{
+    res.send("note is added successfully")
+})
+
+app.post("/api/notes",(req,res)=>{
+    res.send("note is added successfully")
+})
+
 app.listen(5001,()=>{
     console.log("server started on port : 5001");
     
