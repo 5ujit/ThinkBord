f90277acbc83b4988a2af4fac4da618469528539
5ujit <sksujit021@gmail.com>
2026-02-09 23:24:09 +0530

    .

--- CRUD24H/backend/src/server.js
+++ CRUD24H/backend/src/server.js
@@ -1,15 +1,20 @@
 import express from "express";
-import notesRouter from "./routes/notesRoutes.js"
+import notesRouter from "./routes/notesRoutes.js";
+import { connectDB } from "./config/db.js";
+import dotenv from "dotenv"
 
+dotenv.config();
+
+
+console.log(process.env.MONGO_URI);
 
 const app = express();
 
+
+connectDB();
 const PORT = 5001;
 
-app.use("/api/notes",notesRouter);
-
-
-
+app.use("/api/notes", notesRouter);
 
 app.listen(5001, () => {
   console.log(`server started on port : ${PORT}`);
