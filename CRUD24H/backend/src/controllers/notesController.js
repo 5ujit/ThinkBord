71cb2ced191741b24249b2beefa5427d1f6a7564
5ujit <sksujit021@gmail.com>
2026-02-11 21:48:21 +0530

    notes created post

--- CRUD24H/backend/src/controllers/notesController.js
+++ CRUD24H/backend/src/controllers/notesController.js
@@ -1,17 +1,26 @@
-import Note from "../models/Notes.js"
+import Note from "../models/Notes.js";
 
 export async function getAllNotes(req, res) {
- try {
-  const notes = await Note.find()
-  res.status(200).json(notes)
- } catch (error) {
+  try {
+    const notes = await Note.find();
+    res.status(200).json(notes);
+  } catch (error) {
+    console.error("Error in getAllNotes controller", error);
 
-  res.status(500).json({message:"Internal server error"})  
- }
+    res.status(500).json({ message: "Internal server error" });
+  }
 }
 
-export function createNotes(req, res) {
-  res.status(201).json({ message: "post created successfully!" });
+export async function createNotes(req, res) {
+  try {
+    const { title, content } = req.body;
+    const newNote = new Note({ title: title, content: content });
+    await newNote.save();
+    res.status(201).json({ message: "Note created successfully" });
+  } catch (error) {
+    console.error("Error in createNote controller ", error);
+    res.status(500).json({ message: "Internal server error" });
+  }
 }
 
 export function updateNotes(req, res) {
@@ -21,3 +30,5 @@ export function updateNotes(req, res) {
 export function deleteNotes(req, res) {
   res.status(200).json({ message: "notes Deleted  successfully" });
 }
+
+//backend>src> controllers>file notesController.js
--- CRUD24H/backend/src/server.js
+++ CRUD24H/backend/src/server.js
@@ -12,6 +12,8 @@ const PORT = process.env.PORT || 5001;
 
 connectDB();
 // const PORT = 5001;
+// middleware 
+app.use(express.json());
 
 app.use("/api/notes", notesRouter);
 
