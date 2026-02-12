b41a318cb734e54421d0c6d8887d307ed3aac948
5ujit <sksujit021@gmail.com>
2026-02-12 22:12:45 +0530

    updated controller

--- CRUD24H/backend/src/controllers/notesController.js
+++ CRUD24H/backend/src/controllers/notesController.js
@@ -14,18 +14,50 @@ export async function getAllNotes(req, res) {
 export async function createNotes(req, res) {
   try {
     const { title, content } = req.body;
-    const newNote = new Note({ title: title, content: content });
-    await newNote.save();
-    res.status(201).json({ message: "Note created successfully" });
+    const note = new Note({ title: title, content: content });
+    const savedNote = await note.save();
+    res.status(201).json(savedNote);
   } catch (error) {
     console.error("Error in createNote controller ", error);
     res.status(500).json({ message: "Internal server error" });
   }
 }
 
-export function updateNotes(req, res) {
-  res.status(200).json({ message: "notes Updated successfully" });
+
+
+
+
+
+export async function updateNotes(req, res) {
+  try {
+    const { title, content } = req.body;
+    const updatedNote = await Note.findByIdAndUpdate(
+      req.params.id,
+      { title, content },
+      {
+        new: true,
+      }
+    );
+     
+    if (!updatedNote) return res.status(404).json({ message: "Note not found" });
+
+    res.status(200).json({ message: "Note Updated successfully" });
+  } catch (error) {
+    console.error("Error in updateNote controller", error);
+    res.status(500).json({ message: "Internal server error " });
+  }
 }
+
+
+
+
+
+
+
+
+
+
+
 
 export function deleteNotes(req, res) {
   res.status(200).json({ message: "notes Deleted  successfully" });
